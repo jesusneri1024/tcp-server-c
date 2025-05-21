544f37a0edbb1b3e6a9b3c7231ba2ac72f11643c
Jesús Neri <jesusneri1024@outlook.com>
2025-05-21 06:52:36 -0600

    feat: Enabled simultaneous connections from multiple clients

--- main.c
+++ main.c
@@ -3,40 +3,95 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
 
 #define PORT 8080
 #define BUF_SIZE 1024
+
+void manejar_cliente(int cliente_fd)
+{
+    char buffer[BUF_SIZE];
+
+    while (1)
+    {
+        int bytes = recv(cliente_fd, buffer, BUF_SIZE - 1, 0);
+        if (bytes <= 0)
+            break;
+        buffer[bytes] = '\0';
+        printf("Cliente dice: %s", buffer);
+        send(cliente_fd, buffer, bytes, 0); // Eco
+    }
+
+    close(cliente_fd);
+    printf("Cliente desconectado\n");
+}
 
 int main()
 {
     int servidor_fd, cliente_fd;
     struct sockaddr_in direccion;
-    char buffer[BUF_SIZE];
+    socklen_t addr_len = sizeof(direccion);
+
+    // Evitar procesos zombie
+    signal(SIGCHLD, SIG_IGN);
 
     servidor_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (servidor_fd == -1)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
     direccion.sin_family = AF_INET;
     direccion.sin_addr.s_addr = INADDR_ANY;
     direccion.sin_port = htons(PORT);
 
-    bind(servidor_fd, (struct sockaddr *)&direccion, sizeof(direccion));
-    listen(servidor_fd, 5);
-    printf("Servidor esperando en puerto %d...\n", PORT);
+    if (bind(servidor_fd, (struct sockaddr *)&direccion, sizeof(direccion)) < 0)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
 
-    cliente_fd = accept(servidor_fd, NULL, NULL);
-    printf("Cliente conectado\n");
+    if (listen(servidor_fd, 5) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Servidor escuchando en puerto %d...\n", PORT);
 
     while (1)
     {
-        int bytes = recv(cliente_fd, buffer, BUF_SIZE, 0);
-        if (bytes <= 0)
-            break;
-        buffer[bytes] = '\0';
-        printf("Cliente dice: %s", buffer);
-        send(cliente_fd, buffer, bytes, 0); // Echo
+        cliente_fd = accept(servidor_fd, (struct sockaddr *)&direccion, &addr_len);
+        if (cliente_fd < 0)
+        {
+            perror("accept");
+            continue;
+        }
+
+        printf("Nuevo cliente conectado\n");
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            // Proceso hijo
+            close(servidor_fd); // El hijo no necesita el socket del servidor
+            manejar_cliente(cliente_fd);
+            exit(0); // Termina el hijo
+        }
+        else if (pid > 0)
+        {
+            // Proceso padre
+            close(cliente_fd); // El padre no maneja esta conexión
+        }
+        else
+        {
+            perror("fork");
+        }
     }
 
-    close(cliente_fd);
     close(servidor_fd);
     return 0;
 }
